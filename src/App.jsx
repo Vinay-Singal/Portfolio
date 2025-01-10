966fa515be282a5ac2b8f1e30d3f65cf4e796671
VINAY SINGAL <vinaysingal123ghn@gmail.com>
2025-01-10 20:39:19 +0530

    done !!

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
-import Portfolio from './components/Portfolio'
+import Portfolio from './components/Portfolio';
 import Experiance from './components/Experiance'
 import Footer from './components/Footer'
 import Contact from './components/Contact'
